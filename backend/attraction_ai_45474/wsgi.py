fa4807ad08f855f106cfdb48dbaa1c2ad666ae18
Crowdbotics <team@crowdbotics.com>
2024-01-22 22:06:43 +0000

    Initial commit

---
+++ backend/attraction_ai_45474/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for attraction_ai_45474 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "attraction_ai_45474.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "attraction-ai-45474",
+  url: "https://attraction-ai-45474.botics.co",
+  api: "https://attraction-ai-45474.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
